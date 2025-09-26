8ea413dc35bffaab74a42d82ce3f61f637b8463d
Lukas Knopper <lukas.knopper@outlook.com>
2025-09-26 14:17:46 +0200

    werkende functie voor genereren lijst van nootlengtes

--- python/pseudocode_sequencer_python.py
+++ python/pseudocode_sequencer_python.py
@@ -9,42 +9,6 @@ numerator = 7
 denominatorDur = 0.5
 totalDur = numerator * denominatorDur
 
-#bereken de eerstegraads nootlengtes
-# a - b -
-a = random.randint(1, numerator - 1)
-b = numerator - a
-aDur = a * denominatorDur
-bDur = b * denominatorDur
-
-#bereken de tweedegraads nootlengtes
-# a - c - b - d - 
-c = random.randint(1, numerator - 1)
-d = numerator - c
-cDur = (aDur / numerator) * c
-dDur = (bDur / numerator) * d
-
-#bereken de derdegraads nootlengtes
-# a - e - c - f - b - g - d - h -
-e = random.randint(1, numerator - 1)
-f = numerator - e
-g = random.randint (1, numerator - 1)
-h = numerator - g
-eDur = ((aDur - cDur) / numerator) * e
-fDur = (cDur / numerator) * f
-gDur = ((bDur - dDur) / numerator) * g
-hDur = (dDur / numerator) * h
-
-# a - e - c - f - b - g - d - h -
-#fix nootlengtes
-aDur = aDur - (cDur + eDur)
-cDur = cDur - fDur
-bDur = bDur - (dDur + gDur)
-dDur = dDur - hDur
-
-#verzamelen in een lijst
-durArray = [aDur, eDur, cDur, fDur, bDur, gDur, dDur, hDur]
-print(durArray)
-
 #TODO idee om te kleine nootwaarden samen te voegen met vorige noten
 
 #TODO playback mogelijk maken
@@ -54,37 +18,45 @@ print(durArray)
 
 #genereer een nummer om de maat op te delen
 division = random.randrange(1, numerator)/numerator
-
+print(division)
 #voor elke noot 1 for loop formule bedenken
 #aantal recursieve divisions
 depth = 3
 #notenaantal berekenen
 N = (2 ** depth)
-durations = []
+durations = [totalDur]
+
+
+def isEven (value):
+    return (value % 2) == 0
 
 def fractals (depth):
-    if (depth > 0)
-    fractals (depth)
-    durations = [totalDur]
-    
-    for i in range(depth)
-    fractals (depth - 1)
+    if (depth > 0):
+        for i in range(len(durations) * 2):
+            if (isEven(i)):
+                lastValue = durations[i]
+                splitValue = lastValue * division
+                durations[i] = splitValue
+            else:
+                durations.insert(i, (lastValue - splitValue))
+        fractals (depth - 1)
+    else:  
+        print(durations)
+        return durations
+        
+fractals(depth)
 
-for i in range():
-
-    i = 
-    
-    durations = [i]
+   
 
 
-Dur1 = totalDur * division * division * division
-Dur2 = totalDur * division * division * (1 - division)
-Dur3 = totalDur * division * (1 - division) * division
-Dur4 = totalDur * division * (1 - division) * (1 - division)
-Dur5 = totalDur * (1 - division) * division * division
-Dur6 = totalDur * (1 - division) * division * (1 - division)
-Dur7 = totalDur * (1 - division) * (1 - division) * division
-Dur8 = totalDur * (1 - division) * (1 - division) * (1 - division)
+#Dur1 = totalDur * division * division * division
+#Dur2 = totalDur * division * division * (1 - division)
+#Dur3 = totalDur * division * (1 - division) * division
+#Dur4 = totalDur * division * (1 - division) * (1 - division)
+#Dur5 = totalDur * (1 - division) * division * division
+#Dur6 = totalDur * (1 - division) * division * (1 - division)
+#Dur7 = totalDur * (1 - division) * (1 - division) * division
+#Dur8 = totalDur * (1 - division) * (1 - division) * (1 - division)
 
 
 
