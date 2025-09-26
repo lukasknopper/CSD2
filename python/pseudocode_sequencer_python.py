c50d57af05133d3557127b6df43011475e564bbd
Lukas Knopper <lukas.knopper@outlook.com>
2025-09-26 10:12:35 +0200

    pseudocode

--- python/pseudocode_sequencer_python.py
+++ python/pseudocode_sequencer_python.py
@@ -50,3 +50,41 @@ print(durArray)
 #TODO playback mogelijk maken
 # -- speel geluid, wacht
 
+#TODO code herformuleren
+
+#genereer een nummer om de maat op te delen
+division = random.randrange(1, numerator)/numerator
+
+#voor elke noot 1 for loop formule bedenken
+#aantal recursieve divisions
+depth = 3
+#notenaantal berekenen
+N = (2 ** depth)
+durations = []
+
+def fractals (depth):
+    if (depth > 0)
+    fractals (depth)
+    durations = [totalDur]
+    
+    for i in range(depth)
+    fractals (depth - 1)
+
+for i in range():
+
+    i = 
+    
+    durations = [i]
+
+
+Dur1 = totalDur * division * division * division
+Dur2 = totalDur * division * division * (1 - division)
+Dur3 = totalDur * division * (1 - division) * division
+Dur4 = totalDur * division * (1 - division) * (1 - division)
+Dur5 = totalDur * (1 - division) * division * division
+Dur6 = totalDur * (1 - division) * division * (1 - division)
+Dur7 = totalDur * (1 - division) * (1 - division) * division
+Dur8 = totalDur * (1 - division) * (1 - division) * (1 - division)
+
+
+
